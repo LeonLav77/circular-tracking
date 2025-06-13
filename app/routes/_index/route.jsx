9cda36b9fce23a6bbcdcb8c7627bd7933086a2e0
LeonLav77 <leonlav77@gmail.com>
2025-06-13 20:04:22 +0200

    a

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,13 +1,16 @@
+// app/routes/_index/route.jsx
 import { redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-import { login } from "../../shopify.server";
+import { authenticate, login } from "../../shopify.server";
 import styles from "./styles.module.css";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
 
   if (url.searchParams.get("shop")) {
-    throw redirect(`/app?${url.searchParams.toString()}`);
+    // Actually authenticate here instead of just redirecting
+    await authenticate.admin(request);
+    return redirect("/app");
   }
 
   return { showForm: Boolean(login) };
